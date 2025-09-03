1cdc0da2f3ad487b4a1fe5d23b04f240ed7198b4
waber <waberjunior@hotmail.com>
2025-09-02 23:06:39 -0300

    Feat: Enhance authentication configuration with role and name claim types

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
@@ -18,7 +18,6 @@ using Ambev.DeveloperEvaluation.WebApi.Features.Sales.ListSales;
 using Ambev.DeveloperEvaluation.WebApi.Features.Sales.UpdateSale;
 using AutoMapper;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales;
